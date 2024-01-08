0d15048b1f8dcfd19c1ac2ff02b4d4712555eb15
Abhivic <1032210906@tcetmumbai.in>
2024-01-08 20:32:08 +0530

    Day9

--- A_Everybody_Likes_Good_Arrays.cpp
+++ A_Everybody_Likes_Good_Arrays.cpp
@@ -2,22 +2,25 @@
 using namespace std;
 
 int main(){
-    
     int t;
-    cin>>t;
+    cin >> t;
 
     while (t--){
-       int n;
-       cin>>n;
-       vector<int>v(n);
-       for(int i=0; i<n; i++){
-        cin>>v[i];
-       }
-       for(int i=0; i<n; i++){
-         if(v[i]%2==0)
-       }
+         int n;
+    cin >> n;
+    int a[n];
 
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-     
+
+    int ans = 0;
+    for(int i = 0; i + 1 < n; i++) {
+        ans += ((a[i] % 2) == (a[i + 1] % 2));
+        // If both numbers have the same parity, increment the answer
+    }
+
+    cout << ans << "\n";
+    }
     return 0;
 }
