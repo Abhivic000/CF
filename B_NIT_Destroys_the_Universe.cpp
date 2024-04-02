90c476047b4a719649c87700c33945d3073f4ee6
Abhivic <1032210906@tcetmumbai.in>
2024-04-03 00:16:43 +0530

    Day47

---
+++ B_NIT_Destroys_the_Universe.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve() {
+    ll n;
+    cin>>n;
+    vi v(n+1);
+    rep(i,1,n){
+        cin>>v[i];
+    }
+    ll cnt=0;
+    rep(i,1,n){
+        if(v[i-1]==0 && v[i]!=0) cnt++;
+    }
+    cout<<min(cnt,(ll) 2)<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
