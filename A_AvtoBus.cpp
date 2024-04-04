e3c9921ddb461443c12542bdd026962e1e504492
Abhivic <1032210906@tcetmumbai.in>
2024-04-04 21:55:28 +0530

    Day48

---
+++ A_AvtoBus.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve() {
+    ll n;
+    cin>>n;
+    ll maxi,mini;
+    if(n&1 || n<4){
+        cout<<-1<<endl;
+        return;
+    }
+    if(n%6==0){
+     mini=n/6;
+    }else{
+        mini=n/6 +1;
+    }
+    maxi=n/4;
+    cout<<mini<<" "<<maxi<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
