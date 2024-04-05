888cf88bde5b138d25647d30ccb868c8b757ea7d
Abhivic <1032210906@tcetmumbai.in>
2024-04-05 22:33:41 +0530

    Day49-2

---
+++ B_Fair_Numbers.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+bool digiCheck(ll n){
+    ll num=n;
+    while(num!=0){
+        ll x=num%10;
+        if(x!=0 && n%x!=0){
+            return false;
+        }
+        num/=10;
+    }
+    return true;
+}
+
+void solve() {
+    ll n;
+    cin>>n;
+    while(!digiCheck(n)){
+        n++;
+    }
+    cout<<n<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
