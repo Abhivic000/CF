8294bb4238ef07e0930446dc16a31a261ffe7de9
Abhivic <1032210906@tcetmumbai.in>
2024-05-31 21:58:34 +0530

    Day59

---
+++ B_Symmetric_Encoding.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve() {
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string r;
+    unordered_map<char,int>mp;
+    for(int i=0; i<n; i++){
+        if(mp[s[i]]<1){
+            r+=s[i];
+        }
+        mp[s[i]]++;
+    }
+    sort(r.begin(),r.end());
+    ll sz=r.size();
+    for(int i=0; i<n; i++){
+
+    }
+    cout<<r<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
