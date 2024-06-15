6bad564e8bbd7601b0c15ac330ad798a64d8d007
Abhivic <1032210906@tcetmumbai.in>
2024-06-15 23:53:37 +0530

    Day68

---
+++ A_Division.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve() {
+    ll n;
+    cin>>n;
+    if(n>=1900){
+        cout<<"Division 1"<<endl;
+    }else if(n<=1899 && n>=1600){
+        cout<<"Division 2"<<endl;
+    }else if(n<=1599 && n>=1400){
+        cout<<"Division 3"<<endl;
+    }else{
+        cout<<"Division 4"<<endl;
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
