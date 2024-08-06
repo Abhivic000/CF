718c010dcf7eed44b26ce3c7af3d678cf38545bf
Abhivic <1032210906@tcetmumbai.in>
2024-08-07 02:17:59 +0530

    Day82

---
+++ B_Card_Game.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define fr     first
+#define sc     second
+#define pb     push_back
+#define pob    pop_back
+#define pf     push_front
+#define pof    pop_front
+
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+// for (int i = 1; i <= n; i++) { }
+//rep(i,1,n) { }
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+void solve() {
+    ll a1,a2,b1,b2;
+    cin>>a1>>a2>>b1>>b2;
+    ll cnt=0;
+    if(a1>=b1 && a2>b2 || a1>b1 && a2>=b2){
+        cnt++;
+    }
+    if(a1>=b2 && a2>b1 || a1>b2 && a2>=b1){
+        cnt++;
+    }
+    cout<<2*cnt<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+    
+}
